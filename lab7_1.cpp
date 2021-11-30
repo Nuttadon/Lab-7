c3b9e035a879611c0d03881b3e8bf5244937e659
Nuttadon <nuttadon_p@cmu.ac.th>
2021-11-30 15:07:20 +0700

    Lab7_1 complete

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -17,7 +17,7 @@ string func2(string x){
 	int i = 0, L = x.size();
 	string y = "";
 	while(i < L){
-		y += toupper(x[i]);
+		y += toupper(x[i]);//upcase
 		i++;
 	}
 	return y;	
@@ -27,15 +27,27 @@ string func3(string x){
 	int i = 0, L = x.size();
 	string y = "";
 	while(i < L){
-		y += tolower(x[i]);
+		y += tolower(x[i]);//lowcase
 		i++;
 	}
 	return y;	
 }
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+	string comword1,comword2,word,reword;
+    cout << "Input text: ";
+	cin >> word;
+	comword1 = func3(word);
+    cout << "Reversed text: ";
+	reword = func1(word);
+	comword2 = func3(reword);
+	cout << reword << endl;
+    cout << "Palindrome: ";
+	if(comword2 ==comword1){
+		cout << "Yes"; 
+	}
+	else{
+		cout << "No";
+	}
     return 0;
 }
