ee35c10ed0d7262064c828e457a8c56adb42c3eb
Nuttadon <nuttadon_p@cmu.ac.th>
2021-11-30 15:57:12 +0700

    Lab7_3 complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,42 @@
 #include<iostream>
 
 using namespace std;
-
+int adiff(int A,int B){
+  int C=0;
+    if(A>B){
+      C = A-B;
+      while(C>=360){
+        C = C-360;
+      }
+      if(C>180){
+        C=360-C;
+      }
+      return C;
+    }
+    else if(B>A){
+      C = B-A;
+      while(C>=360){
+        C = C-360;
+      }
+      if(C>180){
+        C=360-C;
+      }
+    return C;
+    }
+    else if(A=B){
+      C = 0;
+      return C;
+    }
+  return 0;
+}
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270)<<endl;
+  cout << adiff(210,45)<<endl;
+  cout << adiff(0,360)<<endl;
+  cout << adiff(10,350)<<endl;
+  cout << adiff(95,260)<<endl;
+  cout << adiff(90,-90)<<endl;
+  cout << adiff(1000,280)<<endl;
+  cout << adiff(60,244)<<endl;
 }
